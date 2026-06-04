a3195e0d2d1d1e2f68242ff918b6f6a3d1ee31c3
Karthikadevi Tamilarasu <t.karthikadevi@accenture.com>
2026-06-04 23:17:09 +0530

    Test file minor changes

--- UnitConversion.Tests/ConversionServiceTests.cs
+++ UnitConversion.Tests/ConversionServiceTests.cs
@@ -12,42 +12,42 @@ public class ConversionServiceTests
     [Fact]
     public void Meter_To_Foot_Should_Return_Correct_Value()
     {
-        var result = _service.Convert(1, "meter", "foot");
+        var result = _service.Convertion(1, "meter", "foot");
         Assert.Equal(3.28084, result, 5);
     }
 
     [Fact]
     public void Foot_To_Meter_Should_Return_Correct_Value()
     {
-        var result = _service.Convert(3.28084, "foot", "meter");
+        var result = _service.Convertion(3.28084, "foot", "meter");
         Assert.Equal(1, result, 4);
     }
 
     [Fact]
     public void Kilogram_To_Pound_Should_Return_Correct_Value()
     {
-        var result = _service.Convert(1, "kilogram", "pound");          
+        var result = _service.Convertion(1, "kilogram", "pound");          
         Assert.Equal(2.20462, result, 5);
     }
 
     [Fact]
     public void Pound_To_Kilogram_Should_Return_Correct_Value()
     {
-        var result = _service.Convert(2.20462, "pound", "kilogram");
+        var result = _service.Convertion(2.20462, "pound", "kilogram");
         Assert.Equal(1, result, 4);
     }
 
     [Fact]
     public void Celsius_To_Fahrenheit_Should_Return_32()
     {
-        var result = _service.Convert(0, "celsius", "fahrenheit");
+        var result = _service.Convertion(0, "celsius", "fahrenheit");
         Assert.Equal(32, result);
     }
 
     [Fact]
     public void Fahrenheit_To_Celsius_Should_Return_0()
     {
-        var result = _service.Convert(32, "fahrenheit", "celsius");
+        var result = _service.Convertion(32, "fahrenheit", "celsius");
         Assert.Equal(0, result);
     }   
 }
